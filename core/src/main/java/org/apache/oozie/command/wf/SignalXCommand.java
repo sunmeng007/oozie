4bf03c5f44f3301c32a57e7da704d7e936f8959c
Mona Chitnis <mona@apache.org>
2013-09-17 23:09:07 +0000

    Fork-join mismatch makes workflow Failed but some actions stay Running (mona)
    git-svn-id: https://svn.apache.org/repos/asf/oozie/trunk@1524249 13f79535-47bb-0310-9956-ffa450edef68
    (cherry picked from commit d6e786594e7cca0d768168f41554320067f0ea32)

--- core/src/main/java/org/apache/oozie/command/wf/SignalXCommand.java
+++ core/src/main/java/org/apache/oozie/command/wf/SignalXCommand.java
@@ -181,6 +181,7 @@ public class SignalXCommand extends WorkflowXCommand<Void> {
             }
             catch (WorkflowException e) {
                 wfJob.setStatus(WorkflowJob.Status.valueOf(workflowInstance.getStatus().toString()));
+                completed = true;
             }
             wfJob.setWorkflowInstance(workflowInstance);
             wfAction.resetPending();
